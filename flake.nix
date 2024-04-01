b7e36a9a27b06a900ed12f89de0f346fe39c3b53
Johanno <johanno.dxxxd@gmail.com>
2024-04-01 23:57:54 +0200

    +x

--- flake.nix
+++ flake.nix
@@ -4,20 +4,21 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
     flake-utils.url = "github:numtide/flake-utils";
-    logitech-g110 = {
+    logitech-g110-xyt = {
       type = "github";
       owner = "johanno";
 #       repo = "Linux-G15-Daemon-Logitech-G110-";
       repo = "test";
+      flake = false;
     };
   };
 
-  outputs = { self, nixpkgs, flake-utils, logitech-g110, ... }: {
+  outputs = { self, nixpkgs, flake-utils, logitech-g110-xyt, ... }: {
     defaultPackage.x86_64-linux =
       with import nixpkgs { system = "x86_64-linux"; };
       stdenv.mkDerivation {
-        name = "Logitech-G110";
-         src =  logitech-g110;
+        name = "Logitech-G110-name";
+         src =  logitech-g110-xyt;
           buildInputs = with nixpkgs; [
             autoPatchelfHook
             g15daemon
@@ -27,13 +28,14 @@
           ];
           installPhase = ''
             cd libg15-1.2.7-G110_Patch/
+            ls -al
             ./configure
             make
-            sudo make install
+            make install
             cd ../g15macro-1.0.3
             ./configure
             make
-            sudo make install
+            make install
           '';
     };
 #     flake-utils.lib.eachDefaultSystem (system:
